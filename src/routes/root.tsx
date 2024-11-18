aea3b449d05467beff1d602a06eb8784115829fe
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-18 21:23:50 +0000

    Add root markup

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,19 @@
 export default function Root() {
   return (
     <>
-      <h1>Root</h1>
+      <header>
+        <h2>Booking</h2>
+        <ul>
+          <li>Home</li>
+          <li>Hotels</li>
+        </ul>
+      </header>
+      <main>
+        <h1>Root</h1>
+      </main>
+      <footer>
+        2024 booking
+      </footer>
     </>
   );
 }
