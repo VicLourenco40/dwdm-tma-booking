03690bdb35170d6062a80424b1a185bd07fd3dba
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-18 22:02:40 +0000

    Add link to header logo

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -4,7 +4,7 @@ export default function Root() {
   return (
     <>
       <header>
-        <h2>Booking</h2>
+        <h2><Link to={'/'}>Booking</Link></h2>
         <ul>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'hotels'}>Hotels</Link></li>
