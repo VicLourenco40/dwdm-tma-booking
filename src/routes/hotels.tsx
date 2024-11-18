a1e59a84083ffb8bacd8cf30dc29bbfcc5ca20e4
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-18 22:24:47 +0000

    List hotels

--- src/routes/hotels.tsx
+++ src/routes/hotels.tsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from 'react';
+
+type Hotel = {
+  id: string;
+  name: string;
+  location: string;
+  country: {
+    name: string;
+  }
+  averageReview: number;
+};
+
 export default function Hotels() {
+  useEffect(() => {
+    getHotels();
+  }, []);
+
+  async function getHotels() {
+    await fetch('https://api-tma-2024-production.up.railway.app/hotels')
+      .then(async response => await response.json())
+      .then(data => setHotels(data.hotels));
+  }
+
+  const [hotels, setHotels] = useState<Hotel[]>([]);
+
   return (
     <>
       <h1>Hotels</h1>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Location</th>
+          <th>Country</th>
+          <th>Rating</th>
+        </thead>
+        <tbody>
+          {hotels.map(hotel => (
+            <tr key={hotel.id}>
+              <td>{hotel.name}</td>
+              <td>{hotel.location}</td>
+              <td>{hotel.country.name}</td>
+              <td>{hotel.averageReview}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
