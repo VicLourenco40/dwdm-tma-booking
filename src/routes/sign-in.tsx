de503df048e586282b811c3884acd625808c10cc
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 15:06:29 +0000

    Add sign in form

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -2,6 +2,16 @@ export default function SignIn() {
   return (
     <>
       <h1>Sign In</h1>
+
+      <form>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" required />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" minLength={6} required />
+
+        <input type="submit" value="Sign In" />
+      </form>
     </>
   );
 }
