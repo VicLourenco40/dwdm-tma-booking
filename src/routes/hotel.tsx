87ea8393bedcaad85d6f814445b81562f88357cb
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 00:01:01 +0000

    List hotel properties

--- src/routes/hotel.tsx
+++ src/routes/hotel.tsx
@@ -4,6 +4,17 @@ import { useParams } from 'react-router-dom';
 type Hotel = {
   id: string;
   name: string;
+  description: string;
+  location: string;
+  country: {
+    name: string;
+  };
+  rooms: [
+    {
+      id: string;
+    }
+  ];
+  averageRating: number;
 };
 
 export default function Hotel() {
@@ -16,7 +27,7 @@ export default function Hotel() {
   async function getHotel() {
     await fetch(`https://api-tma-2024-production.up.railway.app/hotels/${hotelId}`)
       .then(async response => await response.json())
-      .then(data => setHotel(data.hotel))
+      .then(data => setHotel(data.hotel));
   }
 
   const [hotel, setHotel] = useState<Hotel | null>(null);
@@ -24,7 +35,28 @@ export default function Hotel() {
   return (
     <>
       {hotel ? (
-        <h1>{hotel.name}</h1>
+        <>
+          <h1>{hotel.name}</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Location</th>
+                <th>Country</th>
+                <th>Rooms</th>
+                <th>Rating</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{hotel.location}</td>
+                <td>{hotel.country.name}</td>
+                <td>{hotel.rooms.length}</td>
+                <td>{hotel.averageRating}</td>
+              </tr>
+            </tbody>
+          </table>
+          <p>{hotel.description}</p>
+        </>
       ) : (
         <p>Loading...</p>
       )}
