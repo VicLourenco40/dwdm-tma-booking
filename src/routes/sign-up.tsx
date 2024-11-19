39ec0bb6afdc1f2918e55dc87bb2233510111a77
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 14:24:24 +0000

    Add user state

--- src/routes/sign-up.tsx
+++ src/routes/sign-up.tsx
@@ -1,8 +1,17 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 type Country = {
   id: string;
   name: string;
+}
+
+type User = {
+  name: string;
+  email: string;
+  password: string;
+  birthDate: string;
+  countryId: string;
+  terms: boolean;
 }
 
 export default function SignUp() {
@@ -16,27 +25,81 @@ export default function SignUp() {
       .then(data => setCountries(data.countries));
   }
 
+  async function handleSignUp(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
   const [countries, setCountries] = useState<Country[]>([]);
+
+  const [user, setUser] = useState<User>({
+    name: '',
+    email: '',
+    password: '',
+    birthDate: '',
+    countryId: '',
+    terms: false
+  });
 
   return (
     <>
       <h1>Sign Up</h1>
 
-      <form>
+      <form onSubmit={handleSignUp}>
         <label htmlFor='name'>Name</label>
-        <input type='text' name='name' id='name' minLength={3} required />
+        <input
+          type='text'
+          name='name'
+          id='name'
+          minLength={3}
+          required
+          onChange={event => (
+            setUser(user => ({...user, name: event.target.value}))
+          )}
+        />
 
         <label htmlFor='email'>Email</label>
-        <input type='email' name='email' id='email' required />
+        <input
+          type='email'
+          name='email'
+          id='email'
+          required
+          onChange={event => (
+            setUser(user => ({...user, email: event.target.value}))
+          )}
+        />
 
         <label htmlFor='password'>Password</label>
-        <input type='password' name='password' id='password' minLength={6} required />
+        <input
+          type='password'
+          name='password'
+          id='password'
+          minLength={6}
+          required
+          onChange={event => (
+            setUser(user => ({...user, password: event.target.value}))
+          )}
+        />
 
         <label htmlFor='birth-date'>Birth date</label>
-        <input type='date' name='birth-date' id='birth-date' required />
+        <input
+          type='date'
+          name='birth-date'
+          id='birth-date'
+          required
+          onChange={event => (
+            setUser(user => ({...user, birthDate: event.target.value}))
+          )}
+        />
 
         <label htmlFor='country'>Country</label>
-        <select name='country' id='country' required>
+        <select
+          name='country'
+          id='country'
+          required
+          onChange={event => (
+            setUser(user => ({...user, countryId: event.target.value}))
+          )}
+        >
           <option value=''>Select</option>
           {countries.map(country => (
             <option key={country.id} value={country.id}>{country.name}</option>
@@ -44,7 +107,15 @@ export default function SignUp() {
         </select>
 
         <label htmlFor="terms">Terms and conditions</label>
-        <input type="checkbox" name="terms" id="terms" required />
+        <input
+          type="checkbox"
+          name="terms"
+          id="terms"
+          required
+          onChange={event => (
+            setUser(user => ({...user, terms: event.target.value === 'on'}))
+          )}
+        />
 
         <input type="submit" value="Sign Up" />
       </form>
