d5eac194afda1873e6c366e2963427c838e0aef2
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 16:33:18 +0000

    Show user or sign in route depending on authentication

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,8 @@
 import { Outlet, Link } from 'react-router-dom';
 
 export default function Root() {
+  const token = localStorage.getItem('token');
+
   return (
     <>
       <header>
@@ -8,9 +10,11 @@ export default function Root() {
         <ul>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'/hotels'}>Hotels</Link></li>
-          <li><Link to={'/user'}>User</Link></li>
-          <li><Link to={'/sign-up'}>Sign Up</Link></li>
-          <li><Link to={'/sign-in'}>Sign In</Link></li>
+          {token ? (
+            <li><Link to={'/user'}>User</Link></li>
+          ) : (
+            <li><Link to={'/sign-in'}>Sign In</Link></li>
+          )}
         </ul>
       </header>
       <main>
