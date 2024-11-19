c5aa38623d8cfbae643ba05a55d3844e752a2351
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 16:19:48 +0000

    Get user data

--- src/routes/user.tsx
+++ src/routes/user.tsx
@@ -1,17 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  country: {
+    name: string;
+  }
+  birthDate: string;
+};
 
 export default function User() {
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (token) {
+      getUser();
+    } else {
       navigate('/sign-in');
     }
   }, []);
 
+  async function getUser() {
+    await fetch('https://api-tma-2024-production.up.railway.app/me', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(async response => ({
+      response,
+      data: await response.json()
+    }))
+    .then(({response, data}) => {
+      console.log(data);
+
+      if (response.status === 200) {
+        setUser(data.user);
+      } else {
+        localStorage.removeItem('token');
+        navigate('/sign-in');
+      }
+    });
+  }
+
   const navigate = useNavigate();
 
-  const isAuthenticated = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
+  const [user, setUser] = useState<User | null>(null);
 
   return (
     <>
