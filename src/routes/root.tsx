4bedc77394d923532540e86632f20cdebc0c86a5
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 16:43:18 +0000

    Always show user route on header

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -10,11 +10,7 @@ export default function Root() {
         <ul>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'/hotels'}>Hotels</Link></li>
-          {token ? (
-            <li><Link to={'/user'}>User</Link></li>
-          ) : (
-            <li><Link to={'/sign-in'}>Sign In</Link></li>
-          )}
+          <li><Link to={'/user'}>User</Link></li>
         </ul>
       </header>
       <main>
