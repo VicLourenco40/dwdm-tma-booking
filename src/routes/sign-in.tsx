977e4b34d91d704973ede22ebc9d94aab9037bca
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 15:15:51 +0000

    Add user state

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,16 +1,49 @@
+import { FormEvent, useState } from 'react';
+
+type User = {
+  email: string;
+  password: string;
+}
+
 export default function SignIn() {
+  async function handleSignIn(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
+  const [user, setUser] = useState<User>({
+    email: '',
+    password: ''
+  });
+
   return (
     <>
       <h1>Sign In</h1>
 
-      <form>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" required />
+      <form onSubmit={handleSignIn}>
+        <label htmlFor='email'>Email</label>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          required
+          onChange={event => (
+            setUser(user => ({...user, email: event.target.value}))
+          )}
+        />
 
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" minLength={6} required />
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          minLength={6}
+          required
+          onChange={event => (
+            setUser(user => ({...user, password: event.target.value}))
+          )}
+        />
 
-        <input type="submit" value="Sign In" />
+        <input type='submit' value='Sign In' />
       </form>
     </>
   );
