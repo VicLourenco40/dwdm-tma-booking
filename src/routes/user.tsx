961655e8ba2b45feee77361f6715b1744d06b4a5
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-19 15:56:06 +0000

    Navigate to sign in if not authenticated

--- src/routes/user.tsx
+++ src/routes/user.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function User() {
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/sign-in');
+    }
+  }, []);
+
+  const navigate = useNavigate();
+
+  const isAuthenticated = localStorage.getItem('token');
+
+
   return (
     <>
       <h1>User</h1>
