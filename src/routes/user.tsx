54afc92a93b63703a2cdd212ab02ce54e3cf5d89
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-21 17:59:07 +0000

    Add change user email feature

--- src/routes/user.tsx
+++ src/routes/user.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { FormEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type User = {
   id: string;
@@ -42,6 +42,35 @@ export default function User() {
     });
   }
 
+  async function handleChangeEmail(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    await fetch('https://api-tma-2024-production.up.railway.app/me/change-email', {
+      method: 'PATCH',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+    .then(async response => ({
+      response,
+      data: await response.json()
+    }))
+    .then(({response, data}) => {
+      console.log(data);
+
+      if (response.status === 200 && user) {
+        setUser({...user, email: email});
+      }
+
+      setMessage(data.message);
+    })
+  }
+
   function handleSignOut() {
     localStorage.removeItem('token');
     navigate('/sign-in');
@@ -52,6 +81,12 @@ export default function User() {
   const token = localStorage.getItem('token');
 
   const [user, setUser] = useState<User | null>(null);
+
+  const [email, setEmail] = useState('');
+
+  const [password, setPassword] = useState('');
+
+  const [message, setMessage] = useState('');
 
   return (
     <>
@@ -74,6 +109,29 @@ export default function User() {
               </tr>
             </tbody>
           </table>
+          <h2>Settings</h2>
+          <h3>Change email</h3>
+          <form onSubmit={handleChangeEmail}>
+            <label htmlFor='email'>New email</label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              required
+              onChange={event => setEmail(event.target.value)}
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              minLength={6}
+              required
+              onChange={event => setPassword(event.target.value)}
+            />
+            <input type='submit' value='Change' />
+          </form>
+          {message && <p>{message}</p>}
           <button onClick={handleSignOut}>Sign Out</button>
         </>
       ) : (
