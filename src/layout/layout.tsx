d746012a23ae889bad76c458a7e9b495666a9323
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-26 14:02:12 +0000

    Add layout markup and styling

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,5 +1,26 @@
+import { Link, Outlet } from 'react-router-dom';
+
+import './layout.css';
+
 export default function Layout() {
   return (
-    <></>
+    <>
+      <header>
+        <h2><Link to={'/'}>Booking</Link></h2>
+        <nav>
+          <ul>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/hotels'}>Hotels</Link></li>
+            <li><Link to={'/user'}>User</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        2024 Booking
+      </footer>
+    </>
   );
 }
