436aaa9cd5871b801b19392bcc25bd13ffeb72cb
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-26 19:34:46 +0000

    Add onClick to Button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,11 +2,12 @@ import styles from './button.module.css';
 
 type ButtonProps = {
   text: string;
+  onClick?: React.MouseEventHandler<HTMLDivElement>;
 }
 
 export function Button(props: ButtonProps) {
   return (
-    <div className={styles.button}>
+    <div className={styles.button} onClick={props.onClick}>
       {props.text}
     </div>
   );
