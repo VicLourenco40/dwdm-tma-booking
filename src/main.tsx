afc882a36121ced577f282d15a3ec110786b9204
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-11-29 11:40:43 +0000

    Fix page imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Layout from './pages/layout/layout';
-import Home from './pages/home';
+import Home from './pages/home/home';
 import Hotels from './pages/hotels/hotels';
 import Hotel from './pages/hotel/hotel';
-import User from './pages/user';
-import SignUp from './pages/sign-up';
-import SignIn from './pages/sign-in';
-import Booking from './pages/booking';
-import Review from './pages/review';
+import User from './pages/user/user';
+import SignUp from './pages/sign-up/sign-up';
+import SignIn from './pages/sign-in/sign-in';
+import Booking from './pages/booking/booking';
+import Review from './pages/review/review';
 import './index.css';
 
 const router = createBrowserRouter([{
