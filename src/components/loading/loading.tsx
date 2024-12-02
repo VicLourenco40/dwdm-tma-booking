8090d1037362f0500397d9249be621b025db3421
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-12-02 17:42:53 +0000

    Center loading component

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -3,7 +3,9 @@ import styles from './loading.module.css';
 export function Loading() {
   return (
     <div className={styles.container}>
-      Loading...
+      <div className={styles.loading}>
+        Loading...
+      </div>
     </div>
   );
 }
