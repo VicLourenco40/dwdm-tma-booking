cf07219b7c783a826d6c64fd6dd8990878651e73
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-12-02 17:38:23 +0000

    Add page buttons to home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,17 @@
+import { Button } from '../../components/button/button';
+import { useNavigate } from 'react-router-dom';
 import styles from './home.module.css';
 
 export function Home() {
+  const navigate = useNavigate();
+
   return (
-    <></>
+    <div className={styles.container}>
+      <h1>Booking</h1>
+      <div className={styles['buttons-container']}>
+        <Button text={'Browse hotels'} onClick={() => navigate('/hotels')} />
+        <Button text={'User profile'} onClick={() => navigate('/user')} />
+      </div>
+    </div>
   );
 }
